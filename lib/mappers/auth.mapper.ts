80399a9f9857de1f221234ebdcf40f5189632253
RizwanSolace <rizwansolace31@gmail.com>
2026-08-03 19:17:20 +0530

    Done

--- lib/mappers/auth.mapper.ts
+++ lib/mappers/auth.mapper.ts
@@ -1,23 +1,16 @@
 import { SignupPayLoad } from "@/types/auth";
-import {  LoginPayload } from "@/types/auth";
+import { LoginPayload } from "@/types/auth";
 import { LoginFormData } from "@/lib/validation/auth";
+import { RegisterFormData } from "@/lib/validation/auth";
 
-
-
-export const toSignupPayload = (data: {
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  password: string;
-  confirmPassword: string;
-}): SignupPayLoad => ({
-  name: `${data.firstName} ${data.lastName}`.trim(),
-  email: data.email,
-  phone: data.phone,
-  password: data.password,
-  confirmPassword: data.confirmPassword,
- 
+export const toSignupPayload = (
+  data: Partial<RegisterFormData>
+): SignupPayLoad => ({
+  name: `${data.firstName ?? ""} ${data.lastName ?? ""}`.trim(),
+  email: data.email ?? "",
+  phone: data.phone ?? "",
+  password: data.password ?? "",
+  confirmPassword: data.confirmPassword ?? "",
 });
 export const toLoginPayload = (
   data: LoginFormData
